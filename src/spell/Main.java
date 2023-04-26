afa9fc18a39e85cdb4a1552be44d2dab5b53ee16
solarqk <90076559+solarqk@users.noreply.github.com>
2023-04-26 16:30:43 -0600

    Update Main.java

--- src/spell/Main.java
+++ src/spell/Main.java
@@ -2,24 +2,13 @@ package spell;
 
 import java.io.IOException;
 
-/**
- * A simple main class for running the spelling corrector. This class is not
- * used by the passoff program.
- */
 public class Main {
 	
-	/**
-	 * Give the dictionary file name as the first argument and the word to correct
-	 * as the second argument.
-	 */
 	public static void main(String[] args) throws IOException {
 		
 		String dictionaryFileName = args[0];
 		String inputWord = args[1];
 		
-		//
-        //Create an instance of your corrector here
-        //
 		ISpellCorrector corrector = new SpellCorrector();
 		
 		corrector.useDictionary(dictionaryFileName);
@@ -30,5 +19,4 @@ public class Main {
 		
 		System.out.println("Suggestion is: " + suggestion);
 	}
-
 }
